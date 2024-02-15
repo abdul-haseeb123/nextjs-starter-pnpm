00d2f4d771292a28861c20181370f8157bfb7ccf
abdul-haseeb123 <syedabdulhaseeb611@gmail.com>
2024-02-15 20:48:56 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -31,7 +31,7 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="./nextjs.svg"
+          src="./next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
